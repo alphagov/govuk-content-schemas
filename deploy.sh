66f6a8733c9961a11b5fa8dd537e412ba645ee78
Christopher Baines <christopher.baines@digital.cabinet-office.gov.uk>
2016-10-27 17:53:17 +0100

    Remove deploy.sh as it is no longer used
    Deployment is done through the standard application deployment process.

--- deploy.sh
+++
@@ -1,7 +0,0 @@
-for server in $(govuk_node_list -c backend); do
-  rsync -r dist/* deploy@$server:/data/apps/publishing-api/shared/govuk-content-schemas/
-done
-
-for server in $(govuk_node_list -c "draft_content_store,content_store"); do
-  rsync -r dist/* deploy@$server:/data/apps/content-store/shared/govuk-content-schemas/
-done
