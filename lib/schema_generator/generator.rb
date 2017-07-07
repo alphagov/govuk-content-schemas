7268c4c21d12bdd157013862e4b2274f4b68e991
Kevin Dew <kevin.dew@digital.cabinet-office.gov.uk>
2017-07-07 11:27:54 +0100

    Reapproach how we generate schemas
    Schemas are now generated from the jsonnet single files. This has
    allowed us to do a few things that improve the schemas:
    
    - No more handmade schemas
    - All formats defined by a single schema
    - Greater degree of control about what goes into the schema
    - Notification and frontend schemas are far more accurate
    - Only the definitions that are used are included
    - Links now have base_path as optional by default - this is accurate,
    but could be problematic

--- lib/schema_generator/generator.rb
+++ lib/schema_generator/generator.rb
@@ -1,31 +1,34 @@
+require "jsonnet"
 require "schema_generator/schema"
-require "schema_generator/handmade_schema_copier"
 require "schema_generator/publisher_content_schema_generator"
 require "schema_generator/publisher_links_schema_generator"
 require "schema_generator/frontend_schema_generator"
 require "schema_generator/notification_schema_generator"
+require "schema_generator/format"
+require "schema_generator/definitions_resolver"
+require "schema_generator/expanded_links"
+require "schema_generator/apply_change_history"
 
 module SchemaGenerator
   module Generator
     # @param schema_name [String] like `generic` or `specialist_document`
-    def self.generate(schema_name)
-      # TODO: Stop using hand-made schemas, they make things confusing.
-      if File.exist?("formats/#{schema_name}/publisher_v2/schema.json")
-        HandmadeSchemaCopier.new.copy_schema(schema_name)
-        return
-      end
+    # @param data [Hash] the data from the format definition
+    def self.generate(schema_name, data)
+      format = Format.new(schema_name, data)
 
-      publisher_content_schema = PublisherContentSchemaGenerator.new(schema_name).generate
+      publisher_content_schema = PublisherContentSchemaGenerator.new(format).generate
       Schema.write("dist/formats/#{schema_name}/publisher_v2/schema.json", publisher_content_schema)
 
-      publisher_links_schema = PublisherLinksSchemaGenerator.new(schema_name).generate
+      publisher_links_schema = PublisherLinksSchemaGenerator.new(format).generate
       Schema.write("dist/formats/#{schema_name}/publisher_v2/links.json", publisher_links_schema)
 
-      frontend_schema = FrontendSchema.new(schema_name, publisher_content_schema, publisher_links_schema).generate
-      Schema.write("dist/formats/#{schema_name}/frontend/schema.json", frontend_schema)
-
-      notification_schema = NotificationSchemaGenerator.new(frontend_schema).generate
+      notification_schema = NotificationSchemaGenerator.new(format).generate
       Schema.write("dist/formats/#{schema_name}/notification/schema.json", notification_schema)
+
+      if format.frontend?
+        frontend_schema = FrontendSchemaGenerator.new(format).generate
+        Schema.write("dist/formats/#{schema_name}/frontend/schema.json", frontend_schema)
+      end
     end
   end
 end
