bf24579f971cab95cd9323bffc6de049d5d42355
Kevin Dew <kevin.dew@digital.cabinet-office.gov.uk>
2017-07-07 11:27:54 +0100

    Reapproach how we generate schemas
    Schemas are now generated from the jsonnet single files. This has
    allowed us to do a few things that improve the schemas:
    
    - No more handmade schemas
    - All formats defined by a single schema
    - Greater degree of control about what goes into the schema
    - Notification and frontend schemas are far more accurate
    - Only the definitions that are used are included
    - Links now have base_path as optional by default - this is accurate,
    but could be problematic

---
+++ lib/schema_generator/expanded_links.rb
@@ -0,0 +1,71 @@
+module SchemaGenerator
+  class ExpandedLinks
+     LINK_TYPES_ADDED_BY_PUBLISHING_API = [
+      # The Publishing API will automatically link to any translations (content
+      # with the same content_id but a different locale).
+      "available_translations",
+
+      # Content items that are linked to with a `parent` link type will automatically
+      # have a `children` link type with those items.
+      "children",
+
+      # Working groups have a `policies` link type containing the policies it is
+      # tagged to.
+      "policies",
+
+      # Content items that are members of a collection will have a `document_collections`
+      # link type
+      "document_collections",
+
+      # Content items that are linked to with a `parent_taxon` link type will automatically
+      # have a `child_taxon` link type with those items.
+      "child_taxons",
+
+      # Content items that are linked to as a `topic` will automatically have a
+      # `topic_content` link type with those items.
+      "topic_content",
+
+      # Content items that are linked to as a `mainstream_browse_page` will automatically
+      # have a `mainstream_browse_content` link type with those items.
+      "mainstream_browse_content",
+    ].freeze
+
+    def initialize(format)
+      @format = format
+    end
+
+    def generate
+      properties = {
+        type: "object",
+        additionalProperties: false,
+        properties: links
+      }
+    end
+
+  private
+
+    attr_reader :format
+
+    def links
+      links = publishing_api_links.merge(content_links).merge(edition_links)
+      Hash[ links.sort_by { |k| k } ]
+    end
+
+    def publishing_api_links
+      LINK_TYPES_ADDED_BY_PUBLISHING_API.each_with_object({}) do |type, memo|
+        memo[type] = {
+          "description" => "Link type automatically added by Publishing API",
+          "$ref": "#/definitions/frontend_links"
+        }
+      end
+    end
+
+    def content_links
+      format.content_links.frontend_properties
+    end
+
+    def edition_links
+      format.edition_links.frontend_properties
+    end
+  end
+end
